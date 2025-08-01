a7234ee782e3f3c8612d426c9a29eb7e974cb18e
Taakemichi <ja12tin345@gmail.com>
2025-08-01 15:28:55 +0530

    arthmatic and alert.

--- 01-variables&datatypes/script.js
+++ 01-variables&datatypes/script.js
@@ -1,29 +1,43 @@
-// -------------Datatypes------------
-/*var myName ="Jatin Sharma";
-const myAge = 19;
-var isStudent = true ;
-console.log(myName);
-console.log(typeof myName);
-console.log(myAge);
-console.log(typeof myAge );
-console.log(isStudent);
-console.log(typeof isStudent );
----variables ----------
-let city = "Delhi";
-const country = "India";
-var pincode = 160041;
+    // -------------Datatypes------------
+    /*var myName ="Jatin Sharma";
+    const myAge = 19;
+    var isStudent = true ;
+    console.log(myName);
+    console.log(typeof myName);
+    console.log(myAge);
+    console.log(typeof myAge );
+    console.log(isStudent);
+    console.log(typeof isStudent );
+    ---variables ----------
+    let city = "Delhi";
+    const country = "India";
+    var pincode = 160041;
 
- city = "chandigarh";   
- pincode = 160042;
-console.log(city);
-console.log(country);
-console.log(pincode);*/
+    city = "chandigarh";   
+    pincode = 160042;
+    console.log(city);
+    console.log(country);
+    console.log(pincode);*/
 
-//----------Arthmatic Operators--------
-const num1 = 25;
-const num2 = 25;
-console.log(num1+num2);
-console.log(num1-num2);
-console.log(num1*num2);
-console.log(num1/num2);
-console.log(num1%num2);
+    //----------Arthmatic Operators--------
+    /*const num1 = 25;
+    const num2 = 25;
+    console.log(num1+num2);
+    console.log(num1-num2);
+    console.log(num1*num2);
+    console.log(num1/num2);
+    console.log(num1%num2);*/
+
+    let num1 = prompt("enter number1")
+    let num2 = prompt("enter number2")
+
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+        let sum = num1 + num2;
+        let diff = num1 - num2;
+        let multi = num1*num2;
+        let divi = num1/num2;
+        let rem =num1%num2;
+        
+        alert("sum="+sum + "diff="+diff + "multi="+multi + "divi="+divi + "rem="+rem);
