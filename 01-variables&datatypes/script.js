13a5c19c4fd8c75a2e377e0e4a4f7c76b9fe291f
Taakemichi <ja12tin345@gmail.com>
2025-08-01 14:16:13 +0530

    practice for var and datatypes

---
+++ 01-variables&datatypes/script.js
@@ -0,0 +1,9 @@
+var myName ="Jatin Sharma";
+const myAge = 19;
+var isStudent = true ;
+console.log(myName);
+console.log(typeof myName);
+console.log(myAge);
+console.log(typeof myAge );
+console.log(isStudent);
+console.log(typeof isStudent );
