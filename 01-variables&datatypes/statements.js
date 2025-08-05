fecfa250bfca67c549b7e08506a41374a225043f
Taakemichi <ja12tin345@gmail.com>
2025-08-05 12:18:56 +0530

    statements done and started loops

--- 01-variables&datatypes/statements.js
+++ 01-variables&datatypes/statements.js
@@ -1,26 +1,76 @@
-switch (key) {
-  case value:
-alert("monday")
+// let choose = prompt("choose from 1 to 7");
+// choose = Number(choose);
+// switch (choose) {
+//   case 1:
+// alert("monday")
+//     break;
+//   case 2:
+// alert("tuesday")
+//     break;
+//   case 3:
+// alert("wednesday")
+//     break;
+//   case 4:
+// alert("thursday")
+//     break;
+//   case 5:
+// alert("friday")
+//     break;
+//   case 6: 
+// alert("satuarday ")
+//     break;
+//   case 7: 
+// alert("sunday")
+//     break;
+  
+//   default:alert("enter valid no.")
+//     break;
+// }
+// console.log(choose);
+
+let choose = prompt("choose from 1 to 12 has  days");
+choose = Number(choose);
+switch (choose) {
+  case 1:
+alert("jan has 31 days")
     break;
-  case value:
-alert("tuesday")
+  case 2:
+alert("feb has 28 days")
     break;
-  case value:
-alert("wednesday")
+  case 3:
+alert("mar has 31 days")
     break;
-  case value:
-alert("thursday")
+  case 4:
+alert("apr has 30 days")
     break;
-  case value:
-alert("friday")
+  case 5:
+alert("may has 31 days")
     break;
-  case value: 
-alert("satuarday ")
-    break;5
-  case value: 
-alert("sunday")
+  case 6: 
+alert("june has 30 days ")
     break;
+  case 7: 
+alert("july has 31 days")
+    break;
+    case 8:
+      alert("aug has 31 days")
+      break;
+      case 9:
+        alert("sept has 30 days")
+        break
+        case 10:
+          alert("oct has 31 days")
+          break
+          case 11:
+            alert("nov has 30 days")
+            break
+            case 12 :
+              alert("dec has 31 day")
+              break
+
   
   default:alert("enter valid no.")
     break;
 }
+console.log(choose);
+
