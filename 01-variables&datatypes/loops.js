6c13373e5aa394f57d0421c6382b358f6e57598b
Taakemichi <ja12tin345@gmail.com>
2025-08-05 14:28:53 +0530

    done

---
+++ 01-variables&datatypes/loops.js
@@ -0,0 +1,17 @@
+// 
+// for(let i = 0 ; i<=50 ; i++ ){
+//     if (i%2===0) {
+//         console.log(i)
+//     }
+    
+// }
+// for(let i=1; i<=10;i++){
+//     console.log(7*i);
+// }
+let i=0
+let sum=i
+while (i<=100  ) {
+   sum += i;
+   i++
+}
+console.log(sum);
